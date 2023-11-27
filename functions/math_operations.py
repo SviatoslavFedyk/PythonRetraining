08fdf132dcffdad0ec5c5d479a78f5bcc792964c
Sviatoslav Fedyk <sviatoslav.fedyk@teladochalth.com>
2023-11-27 21:04:55 +0200

    Second task

--- functions/math_operations.py
+++ functions/math_operations.py
@@ -1,2 +1,26 @@
 def is_whole_div(num1, num2):
     return num1 % num2 == 0
+
+def max_ocurrence(arr):
+    occurrences = {num: arr.count(num) for num in set(arr)}
+    return max(occurrences.values())
+
+def max_occurrence_elements(arr):
+    occurrences = {num: arr.count(num) for num in set(arr)}
+    return sorted([num for num, count in occurrences.items() if count == max(occurrences.values())])
+
+def once_occurrence_amount(arr):
+    return len([x for x in set(arr) if arr.count(x) == 1])
+def stat(arr):
+    if not isinstance(arr, list) or not arr or any(isinstance(x, float) for x in arr):
+        return [0, 0, 0, [], 0]
+
+    total_amount = len(arr)
+    unique_amount = len(set(arr))
+    once_occ_amount = once_occurrence_amount(arr)
+
+    max_occ = max_ocurrence(arr)
+    max_occ_elem = max_occurrence_elements(arr)
+
+    result = [total_amount, unique_amount, once_occ_amount, max_occ_elem, max_occ]
+    return result
--- tests/unit/math_functions_test.py
+++ tests/unit/math_functions_test.py
@@ -5,3 +5,10 @@ import pytest
                                                          (25, 5, True), (7, 2, False), (100, 10, True)])
 def test_is_whole_div(num1, num2, expected_result):
     assert task_func.is_whole_div(num1, num2) == expected_result
+
+@pytest.mark.parametrize("arr, expected_result", [ ([1,2,4,4,6,6,3,3,34,65,7], [11, 8, 5, [3, 4, 6], 2]),
+                                                   ([-6,34,34,6,0], [5, 4, 3, [34], 2]),
+                                                   ([-6,34.15,34.15,6,0], [0, 0, 0, [], 0]),
+                                                    (3,[0, 0, 0, [], 0])])
+def test_stat(arr, expected_result):
+    assert task_func.stat(arr) == expected_result
