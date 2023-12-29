aeecc902ddfbeb1e24ab655e46b2ad28f5b98d23
Svyatoslav Fedyk <42944711+Coffezka@users.noreply.github.com>
2023-12-29 14:01:35 +0200

    decorators

--- src/main.py
+++ src/main.py
@@ -3,10 +3,24 @@ from serializers.beer_serializer import serialize_beer_class, serialize_beer_nam
 
 file_path = 'data/Beer_recipe_orignal.json'
 serializer_method = None
-def choose_exit(user_input):
-    if not user_input or user_input.lower() == 'exit':
-        print("Exiting")
-        exit()
+
+
+def exit_if_needed(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if not callable(result) and (not result or result.lower() == 'exit'):
+            print("Exiting.")
+            exit()
+        return result
+
+    return wrapper
+
+
+@exit_if_needed
+def get_user_input(prompt):
+    return input(prompt)
+
+@exit_if_needed
 def choose_serializer():
     print("Choose a serializer method:")
     print("1. Class Serializer")
@@ -14,13 +28,13 @@ def choose_serializer():
     print("3. Dynamic Class Serializer")
     print("4. Exit")
 
-    serializer_choice = input("Enter the number of your choice: ")
+    serializer_choice = get_user_input("Enter the number of your choice: ")
 
     serializer_methods = {
         '1': serialize_beer_class,
         '2': serialize_beer_namedtuple,
         '3': serialize_beer_dynamic_class,
-        '4': exit
+        '4': 'exit'
     }
 
     return serializer_methods.get(serializer_choice.lower(), None)
@@ -31,7 +45,6 @@ while not serializer_method:
     if serializer_method is None:
         print("Invalid choice. Try again.")
 
-
 beers = read_json_file(file_path, serializer_method)
 
 while True:
@@ -39,15 +52,15 @@ while True:
     for beer in beers:
         print(f'{beer.id}.{beer.name}')
 
-    print('\n\nInput the ID of the beer (type "exit" to exit app or input "serializer" to choose different serializer):')
-    input_id = input()
-    choose_exit(input_id)
+    print(
+        '\n\nInput the ID of the beer (type "exit" to exit app or input "serializer" to choose different serializer):')
+    input_id = get_user_input("")
 
     if input_id.lower() == 'serializer':
         serializer_method = choose_serializer()
         if serializer_method is None:
             print("Invalid choice. Try again.")
-    
+
         beers = read_json_file(file_path, serializer_method)
     else:
         try:
@@ -58,13 +71,10 @@ while True:
                 print(f'Name: {selected_beer.name}')
                 print(f'Description: {selected_beer.description}')
                 print('Press enter to continue (type "exit" to exit app):')
-                user_input = input()
-                choose_exit(user_input)
+                user_input = get_user_input("")
             else:
                 print('Invalid beer ID. Press enter to continue (type "exit" to exit app):')
-                user_input = input()
-                choose_exit(user_input)
+                user_input = get_user_input("")
         except ValueError:
-            print('Invalid input. Please enter a valid beer ID (type "exit" to exit app):')
-            user_input = input()
-            choose_exit(user_input)
+            print('Invalid input. Please enter to continue(type "exit" to exit app):')
+            user_input = get_user_input("")
